0bc1853a8ce6cb8236f2f1bc36619a074fae4a2e
liem nguyen <elminguyen@gmail.com>
2024-06-05 10:56:52 +0300

    update the page with category-menu comp and category-item comp

---
+++ src/components/category-menu/category-menu.component.tsx
@@ -0,0 +1,15 @@
+import { Category } from "../../types/DBTypes";
+import CategoryItem from "../category-item/category-item.component";
+import './caategory-menu.styles.scss';
+
+const CategoryMenu = ({ categories }: { categories: Category[] }) => {
+  return (
+    <div className="category-menu">
+        {categories.map((category) => (
+          <CategoryItem key={category.id} category={category}/>
+        ))}
+    </div>
+  );
+};
+
+export default CategoryMenu;
