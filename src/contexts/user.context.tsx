38c466c0f7ee4fd21ae34a7347fb3df5e1506847
liem nguyen <elminguyen@gmail.com>
2024-06-28 09:52:48 +0300

    convert to user reducer

--- src/contexts/user.context.tsx
+++ src/contexts/user.context.tsx
@@ -1,18 +1,62 @@
-import React, { createContext, useEffect, useState } from "react";
+import React, { createContext, useEffect, useReducer } from "react";
 import { createUserDocumentFromAuth, onAuthStateChangedListener } from "../utils/firebase/firebase.utils";
 import { User } from "firebase/auth";
 
+type UserContextType = {
+  currentUser: User | null;
+};
+
 // as the actual value you want to access
-export const UserContext = createContext({
-  setCurrentUser: () => null,
+export const UserContext = createContext<UserContextType>({
+  // setCurrentUser: (user: User) => null,
   currentUser: null,
 });
+
+type UserAction = {
+  type: 'SET_CURRENT_USER';
+  payload?: User;
+};
+
+type UserState = {
+  currentUser: User | null;
+};
+
+const USER_ACTION_TYPES = {
+  SET_CURREN_USER: 'SET_CURRENT_USER',
+}
+
+const initialState: UserState = {
+  currentUser: null,
+}
+
+
+const userReducer = (state: typeof initialState, action: UserAction): UserState => {
+  const { type, payload } = action;
+  console.log(action);
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURREN_USER:
+      return {
+        ...state,
+        currentUser: payload
+      };
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`)
+  }
+
+};
+
 
 
 // actual component
 export const UserProvider = ({children} : {children: React.ReactNode}) => {
-  const [currentUser, setCurrentUser] = useState< User | null>(null);
-  const value = { currentUser, setCurrentUser };
+  const [state, dispatch]= useReducer(userReducer, initialState);
+
+  const { currentUser } = state;
+
+  const setCurrentUser = (user: User) => {
+    // dispatch the action to reducer, includes type and payload
+    dispatch({type: USER_ACTION_TYPES.SET_CURREN_USER, payload: user});
+  };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user: User) => {
@@ -24,5 +68,18 @@ export const UserProvider = ({children} : {children: React.ReactNode}) => {
     return unsubscribe;
   }, []);
 
+  const value = {
+    currentUser,
+  }
+
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>
 };
+
+/*
+const reducer = (state, action) => {
+  return {
+    currenUser
+  }
+}
+
+ */
